779dc752f4054302ab23d8711de53790b9a9c944
Andreas Morgner <andreas.morgner@am-webdev.de>
2016-11-03 18:32:02 +0100

    added some constraints tests for Auction

--- src/de/sb/broker/model/AuctionEntityTest.java
+++ src/de/sb/broker/model/AuctionEntityTest.java
@@ -2,13 +2,71 @@ package de.sb.broker.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+
 import org.junit.Test;
 
 public class AuctionEntityTest extends EntityTest {
 	
 	@Test
 	public void testConstraints() {
-		// TODO
+		Auction auction1 = new Auction();
+		
+		auction1.setTitle("Auction to test");
+		auction1.setDescription("My FooBar Description");
+		auction1.setClosureTimestamp(1482537600);		// 2016-12-24T00:00:00+00:00 
+		auction1.setUnitCount((short) 1);
+		auction1.setAskingPrice(1337);
+		auction1.setSeller(new Person());
+		auction1.setVersion(1);
+		
+		// Some basic tests
+		assertEquals("Auction to test", auction1.getTitle());
+		assertEquals("My FooBar Description", auction1.getDescription());
+		assertEquals(1482537600, auction1.getClosureTimestamp());
+		//assertEquals((short) 1, auction1.getUnitCount());
+		assertEquals(1337, auction1.getAskingPrice());
+		assertNotEquals(null, auction1.getSeller());
+		assertEquals(1, auction1.getVersion());
+		
+		
+		// Legal Test Cases
+		
+		auction1.setAskingPrice(1);
+	    Set<ConstraintViolation<Auction>> constraintViolations = getEntityValidatorFactory().getValidator().validate( auction1 );
+	    assertEquals( 0, constraintViolations.size() );	
+
+		auction1.setAskingPrice(Long.MAX_VALUE);
+		getEntityValidatorFactory().getValidator().validate( auction1 );
+	    assertEquals( 0, constraintViolations.size() );
+
+	    auction1.setDescription("x");
+		getEntityValidatorFactory().getValidator().validate( auction1 );
+	    assertEquals( 0, constraintViolations.size() );
+	    //TODO
+		
+		// Negative Test Cases
+
+		auction1.setAskingPrice(0);
+		Set<ConstraintViolation<Auction>> constraintViolationsZero = getEntityValidatorFactory().getValidator().validate( auction1 );
+	    assertEquals( 1, constraintViolationsZero.size() );
+	    
+	    auction1.setAskingPrice(-1);
+	    auction1.setUnitCount((short) -1);
+		Set<ConstraintViolation<Auction>> constraintViolationsNeg = getEntityValidatorFactory().getValidator().validate( auction1 );
+	    assertEquals( 2, constraintViolationsNeg.size() );
+	    
+	    auction1.setAskingPrice(1);
+	    auction1.setUnitCount((short) 1);
+	    auction1.setTitle("");
+		Set<ConstraintViolation<Auction>> constraintViolationsEmtpyTitle = getEntityValidatorFactory().getValidator().validate( auction1 );
+	    assertEquals( 1, constraintViolationsEmtpyTitle.size() );
+		
+	    // TODO
+		
+		
 	}
 	
 	@Test
--- src/de/sb/broker/model/BidEntityTest.java
+++ src/de/sb/broker/model/BidEntityTest.java
@@ -15,7 +15,8 @@ public class BidEntityTest extends EntityTest {
 		// Generate a new Bid object
 		Bid testBidDefaultConstructor = new Bid();
 		testBidDefaultConstructor.setPrice(42);
-		
+
+		// Some basic tests
 		assertEquals(null, testBidDefaultConstructor.getAuction());
 		assertEquals(null, testBidDefaultConstructor.getBidder());
 		assertEquals(42, testBidDefaultConstructor.getPrice());
@@ -27,44 +28,30 @@ public class BidEntityTest extends EntityTest {
 		assertNotEquals(null, testBid2.getBidder());
 		assertEquals(4711, testBid2.getPrice());
 		
-		/*
-		 * There seams to be an issue within our annotations:
-		 * http://forum.spring.io/forum/spring-projects/roo/81387-no-validator-for-type-long
-		 * If we remove the @Size(min = 1) the validation factory is able to validate otherwise an error is thrown:
-		 * No validator could be found for type: java.lang.Long
-		 */
-		
 		// Legal Test Cases
 		Bid legalValueBid = new Bid();
 		
 		legalValueBid.setPrice(1);
 	    Set<ConstraintViolation<Bid>> constraintViolationsMin = getEntityValidatorFactory().getValidator().validate( legalValueBid );
 	    assertEquals( 0, constraintViolationsMin.size() );		
-
 	    
 		legalValueBid.setPrice(Long.MAX_VALUE);
 		Set<ConstraintViolation<Bid>> constraintViolationsMax = getEntityValidatorFactory().getValidator().validate( legalValueBid );
 	    assertEquals( 0, constraintViolationsMax.size() );		
-
 		
 		// Negative Test Cases
 		Bid negativValueBid = new Bid();
 		negativValueBid.setPrice(-100);
 
 	    Set<ConstraintViolation<Bid>> constraintViolations1 = getEntityValidatorFactory().getValidator().validate( negativValueBid );
-
 	    assertEquals( 1, constraintViolations1.size() );
-	    assertEquals( "The price needs to start at 1ct", constraintViolations1.iterator().next().getMessage());
 		
 		// Price for a bid was set to 0 Test Cases
 		Bid toLowValueBid = new Bid();
-		negativValueBid.setPrice(0);
+		toLowValueBid.setPrice(0);
 
 	    Set<ConstraintViolation<Bid>> constraintViolations2 = getEntityValidatorFactory().getValidator().validate( toLowValueBid );
-
-	    assertEquals( 1, constraintViolations2.size() );
-	    assertEquals( "The price needs to start at 1ct", constraintViolations2.iterator().next().getMessage());
-		
+	    assertEquals( 1, constraintViolations2.size() );		
 	}
 	
 	@Test
