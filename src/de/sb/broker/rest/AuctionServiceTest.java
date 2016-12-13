f315a6c5a5157b3a5a889f9ba44c144f4115030e
Andreas Morgner <andreas.morgner@am-webdev.de>
2016-12-13 16:19:03 +0100

    added TODOs for reg. tests

--- src/de/sb/broker/rest/AuctionServiceTest.java
+++ src/de/sb/broker/rest/AuctionServiceTest.java
@@ -6,16 +6,31 @@ import org.junit.Test;
 
 public class AuctionServiceTest extends ServiceTest {
 
+	/**
+	 * Tests for the method AuctionService::getAuctions()
+	 * TODO Test the query parameter
+	 * TODO Test Exceptions 
+	 */
 	@Test
 	public void testCriteriaQueries() {
 		
 	}
 	
+	/**
+	 * Tests for all methods using a "identity" as a path parameter
+	 * TODO GET "services/auctions/{identity}"
+	 * TODO GET "services/auctions/{identity}/bid"
+	 */
 	@Test
 	public void testIdentityQueries() {
 		
 	}
 	
+	/**
+	 * Tests for 
+	 * TODO POST "services/auctions/{identity}/bid"
+	 * TODO PUT "services/auctions/"
+	 */
 	@Test
 	public void testBidRelations() {
 		
--- src/de/sb/broker/rest/PersonServiceTest.java
+++ src/de/sb/broker/rest/PersonServiceTest.java
@@ -6,26 +6,50 @@ import org.junit.Test;
 
 public class PersonServiceTest extends ServiceTest {
 
+	/**
+	 * Tests for the method PersonService::getPeople()
+	 * TODO Test the query parameter
+	 * TODO Test Exceptions 
+	 */
 	@Test
 	public void testCriteriaQueries() {
 		
 	}
 	
+	/**
+	 * Tests for 
+	 * TODO GET "services/people/{identity}"
+	 * TODO GET "services/people/requester"
+	 * TODO GET "services/people/{identity}/avatar"
+	 */
 	@Test
 	public void testIdentityQueries() {
 		
 	}
 	
+	/**
+	 * Tests for
+	 * TODO GET "services/people/{identity}/auctions"
+	 */
 	@Test
 	public void testAuctionRelationQueries() {
 		
 	}
 	
+	/**
+	 * Tests for
+	 * TODO GET "services/people/{identity}/bids"
+	 */
 	@Test
 	public void testBidRelationQueries() {
 		
 	}
 	
+	/**
+	 * Tests for
+	 * TODO PUT "services/people/"
+	 * TODO PUT "services/people/{identity}/avatar"
+	 */
 	@Test
 	public void testLifeCycle() {
 		
