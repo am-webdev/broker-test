71a251a9546b2d619c2b045872a52eb91e324ce2
Thilo Ilg <contact@thiloilg.com>
2016-12-16 00:42:26 +0100

    some criteria queries for person

--- src/de/sb/broker/rest/PersonServiceTest.java
+++ src/de/sb/broker/rest/PersonServiceTest.java
@@ -2,9 +2,32 @@ package de.sb.broker.rest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.junit.Test;
 
+import de.sb.broker.model.Address;
+import de.sb.broker.model.Auction;
+import de.sb.broker.model.Contact;
+import de.sb.broker.model.Document;
+import de.sb.broker.model.Name;
+import de.sb.broker.model.Person;
+import de.sb.broker.model.Person.Group;
+import junit.framework.Assert;
+
 public class PersonServiceTest extends ServiceTest {
+	
+	
 
 	/**
 	 * Tests for the method PersonService::getPeople()
@@ -14,6 +37,58 @@ public class PersonServiceTest extends ServiceTest {
 	 */
 	@Test
 	public void testCriteriaQueries() {
+		/**
+		 * test criteria queries
+		 */
+		Response response = null;
+		List<Person> l = null;
+		
+		// version
+		final int lowerVersion = 1, upperVersion = 100;
+		response = newWebTarget("root", "root")
+				.path("people")
+				.queryParam("lowerVersion", lowerVersion)
+				.queryParam("upperVersion", upperVersion)
+				.request()
+				.accept(MediaType.APPLICATION_JSON)
+				.get();
+		
+		l = response.readEntity(new GenericType<List<Person>>() {});
+		for(Person p : l){
+			assertTrue(lowerVersion <= p.getVersion());
+			assertTrue(upperVersion >= p.getVersion());
+		}
+		assertEquals(200, response.getStatus());
+		
+		// creationTimeStamp
+		final long lowerCreationTimeStamp = 0, upperCreationTimeStamp = 100;
+		response = newWebTarget("root", "root")
+				.path("people")
+				.queryParam("lowerCreationTimeStamp", lowerCreationTimeStamp)
+				.queryParam("upperCreationTimeStamp", upperCreationTimeStamp)
+				.request()
+				.accept(MediaType.APPLICATION_JSON)
+				.get();
+		
+		l = response.readEntity(new GenericType<List<Person>>() {});
+		for(Person p : l){
+			assertTrue(lowerCreationTimeStamp <= p.getCreationTimeStamp());
+			assertTrue(upperCreationTimeStamp >= p.getCreationTimeStamp());
+		}
+		assertEquals(200, response.getStatus());
+		
+		// alias
+		final String alias = "T-High";
+		response = newWebTarget("root", "root")
+				.path("people")
+				.queryParam("alias", alias)
+				.request()
+				.accept(MediaType.APPLICATION_JSON)
+				.get();
+		
+		l = response.readEntity(new GenericType<List<Person>>() {});
+		assertEquals("alias wrong", alias, l.get(0).getAlias());
+		assertEquals(200, response.getStatus());
 		
 	}
 	
