59bb8b2c3176742fe30785bc205c887e5439c8a6
Martin <spitfire-24@web.de>
2016-12-16 09:51:52 +0100

    change PersonServiceTest

--- src/de/sb/broker/rest/PersonServiceTest.java
+++ src/de/sb/broker/rest/PersonServiceTest.java
@@ -2,7 +2,23 @@ package de.sb.broker.rest;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.transaction.TransactionalException;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Invocation.Builder;
+
 import org.junit.Test;
+
+import de.sb.broker.model.Auction;
+import de.sb.broker.model.Bid;
 
 public class PersonServiceTest extends ServiceTest {
 
@@ -40,8 +56,15 @@ public class PersonServiceTest extends ServiceTest {
 	 * @author Martin
 	 */
 	@Test
-	public void testAuctionRelationQueries() {
-		
+	public void testAuctionRelationQueries() throws Exception {
+		try {	
+			WebTarget webTarget = newWebTarget("sascha", "sascha").path("people/2/auctions").queryParam("title", "Rennrad wie neu");
+			Response response = webTarget.request().accept(MediaType.APPLICATION_JSON).get();
+			List<Auction> all = response.readEntity(new GenericType<List<Auction>>() {}); //Unmarshalling Error
+			assertEquals("Rennrad wie neu", all.get(0).getTitle());
+		} catch(NoResultException e){
+			throw new ClientErrorException(e.getMessage(), 404);
+		}
 	}
 	
 	/**
@@ -54,7 +77,15 @@ public class PersonServiceTest extends ServiceTest {
 	 */
 	@Test
 	public void testBidRelationQueries() {
-		
+		try {	
+			WebTarget webTarget = newWebTarget("sascha", "sascha").path("people/2/bids");
+			Response response = webTarget.request().get();
+			//System.out.println(response.getStatus());
+			List<Bid> all = response.readEntity(new GenericType<List<Bid>>() {});
+			assertEquals(1, all.get(0).getPrice());
+		} catch(NoResultException e){
+			throw new ClientErrorException(e.getMessage(), 404);
+		}
 	}
 	
 	/**
